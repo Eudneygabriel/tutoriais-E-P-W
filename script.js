25baa5a5b4b62d2d462c4ddfb7f51c8dead17aec
Eudneygabriel <eudneygabriel@epfundao.edu.pt>
2024-05-09 14:33:42 +0100

    3 commit

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ document.addEventListener("DOMContentLoaded", function () {
     containerId,
     imageId1,
     imageId2,
-    notificationId
+    notificationId,
+    url
   ) {
     const container = document.getElementById(containerId);
     const image1 = document.getElementById(imageId1);
@@ -13,32 +14,47 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Manipulador de evento de mouse sobre o container
     container.addEventListener("mouseover", function () {
-      // Quando o mouse está sobre o container, a imagem secundária cobre completamente a imagem inicial
       image1.style.opacity = 0;
-      image2.style.left = "0%"; // Ajusta a posição da imagem secundária para cobrir completamente o container
-      // Mostra a notificação com animação suave
+      image2.style.left = "0%";
       notification.style.transform = "translateY(0)";
     });
 
     // Manipulador de evento de saída do mouse
     container.addEventListener("mouseleave", function () {
-      // Quando o mouse sai do container, a imagem inicial volta a ser visível
       image1.style.opacity = 1;
-      // A imagem secundária retorna à sua posição inicial
       image2.style.left = "-100%";
-      // Esconde a notificação com animação suave
       notification.style.transform = "translateY(-100%)";
     });
 
-    // Manipulador de evento de clique para abrir uma nova página
+    // Manipulador de evento de clique para abrir uma nova página na mesma janela ou guia
     container.addEventListener("click", function () {
-      // Abre uma nova janela ou guia com o URL fornecido
-      window.open(url, "_blank");
+      if (url) {
+        // Abre a URL na mesma janela ou guia
+        window.location.href = url;
+      }
     });
   }
 
   // Configurar eventos para cada container de animação
-  configurarEventoDeMouse("container1", "image1", "image2", "notification");
-  configurarEventoDeMouse("container2", "image3", "image4", "notification2");
-  configurarEventoDeMouse("container3", "image5", "image6", "notification3");
+  configurarEventoDeMouse(
+    "container1",
+    "image1",
+    "image2",
+    "notification1",
+    "tutorialexcel.html"
+  );
+  configurarEventoDeMouse(
+    "container2",
+    "image3",
+    "image4",
+    "notification2",
+    "tutorialpowerpoint.html"
+  );
+  configurarEventoDeMouse(
+    "container3",
+    "image5",
+    "image6",
+    "notification3",
+    "tutorialword.html"
+  );
 });
